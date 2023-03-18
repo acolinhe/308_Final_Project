0647dbfde69cc208d113b9c14b50f556df5942eb
acolinhe <acolinhe@gmail.com>
2023-03-18 16:14:34 -0700

    Added shape

---
+++ src/Shape.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+
+public abstract class Shape {
+    int x1, y1, x2, y2;
+    Color c;
+
+    public Shape (int x1, int y1, int x2, int y2, Color c) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.c = c;
+    }
+
+    public abstract void draw(Graphics g);
+}
