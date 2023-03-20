9829913144ea09efb4c34055f6e756c72df8e875
Luke Fanguna <47843203+Luke-Fanguna@users.noreply.github.com>
2023-03-19 23:55:09 -0700

    Add files via upload

--- src/Line.java
+++ src/Line.java
@@ -1,4 +1,53 @@
+import java.awt.*;
+import java.util.*;
+
 public class Line implements LineInterface
 {
+    Stack<String> message;
+    Line()
+    {
+        message.push(" ");
+        message.push("true");
+        message.push("false");
+    }
+    public void draw(Graphics g)
+    {
+        Stack<Shape> connect = Repository.getR().getC();
+        Point start = center(connect.get(0));
+        Point end = center(connect.get(1));
+        g.drawLine(start.x, start.y, end.x, end.y);
 
+        // Calculate the midpoint and angle of the line segment
+        int midX = (start.x + end.x) / 2;
+        int midY = (start.y + end.y) / 2;
+        double angle = Math.atan2(end.y - start.y, end.x - start.x);
+
+        // Calculate the coordinates of the arrowhead vertices
+        int arrowSize = 10;
+        int arrowAngle = 30; // in degrees
+        double arrowLength = arrowSize / Math.sin(Math.toRadians(arrowAngle));
+        int arrowX1 = (int)(midX - arrowLength * Math.cos(angle - Math.toRadians(arrowAngle)));
+        int arrowY1 = (int)(midY - arrowLength * Math.sin(angle - Math.toRadians(arrowAngle)));
+        int arrowX2 = (int)(midX - arrowLength * Math.cos(angle + Math.toRadians(arrowAngle)));
+        int arrowY2 = (int)(midY - arrowLength * Math.sin(angle + Math.toRadians(arrowAngle)));
+
+        // Draw the arrowhead polygon
+        int[] arrowX = {midX, arrowX1, arrowX2};
+        int[] arrowY = {midY, arrowY1, arrowY2};
+        g.fillPolygon(arrowX, arrowY, 3);
+    }
+    private Point center(Shape x) {
+        return new Point(x.x1 + (x.x1 + x.x2) / 2, x.y1 + (x.x2 + x.y2) / 2);
+    }
+    public void lineMessage(Graphics g)
+    {
+        Stack<Shape> connect = Repository.getR().getC();
+        Point start = center(connect.get(0));
+        Point end = center(connect.get(1));
+        if (connect.get(0).equals("Diamond")) {
+            String text = message.pop();
+            g.drawString(text, (start.x-end.x)/2,(start.y-end.y)/2);
+            message.push(text);
+        }
+    }
 }
--- src/Repository.java
+++ src/Repository.java
@@ -1,32 +1,44 @@
-import java.awt.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.Observable;
 public class Repository extends Observable {
-    private Shape shape;
-    private Stack<Shape> shapes;
+    private String shape;
+    private Stack<Shape> shapes = new Stack<Shape>();
     private Line line;
-    private Stack<Line> lines;
+    private Stack<Line> lines = new Stack<Line>();
+    private Stack<Shape> connection = new Stack<Shape>();
     private static Repository repo;
     private String textbox;
 
     /**
      * allows Repository to not be changed out of scope
      */
-    private Repository() {}
+    private Repository(){}
     /**
      * allows us to get the instance of the Respository
      *
      * @return instance of Repository
      */
-    public static Repository getR() {
+    public static Repository getR()
+    {
+        if (repo == null)
+        {
+            repo = new Repository();
+        }
         return repo;
     }
-    public void setShape(Shape shape) {this.shape = shape;}
+    public void setShape(String shape) {this.shape = shape;}
     public void addShape(Shape shape) {shapes.push(shape);}
     public void setLine(Line line) {this.line = line;}
-    public void addLine(Shape line) {lines.push(line);}
+    public void addLine(Line line) {lines.push(line);}
     public void setTextbox(String textbox) {this.textbox = textbox;}
-    public void getTextbox(){return textbox;}
+    public void addC(Shape shape){connection.push(shape);}
+    public Stack<Shape> getShapes() {return shapes;}
+    public Stack<Line> getLines() {return lines;}
+    public Stack<Shape> getC() {return connection;}
+    public void clearC() {connection.clear();}
+    public String getTextbox(){return textbox;}
+    public int getShapeSize(){return shapes.size();}
+    public Shape getElement(int i){return shapes.get(i);}
+    public String getShape(){return shape;}
 
 }
