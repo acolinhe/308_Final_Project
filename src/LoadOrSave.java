e5a93af43fbe5e2d9f4a33d2b475175c1efd8035
ltfranks <114035259+ltfranks@users.noreply.github.com>
2023-03-22 16:00:03 -0700

    Add files via upload

---
+++ src/LoadOrSave.java
@@ -0,0 +1,40 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.util.Stack;
+
+/**
+ * This class provides methods for saving and loading a list of MyShape objects to and from a file.
+ */
+
+public class LoadOrSave {
+
+    /**
+     * Saves a stack of shapes to a file specified by the filePath parameter.
+     * @param all the stack of shapes to be saved
+     * @param filePath the file path to which the shapes will be saved
+     * @throws IOException if an I/O error occurs while writing to the file
+     */
+    public static void saveObjects(Stack<Object> all, String filePath) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(filePath);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(all);
+        }
+    }
+
+    /**
+     * Loads a stack of shapes from a file specified by the filePath parameter.
+     * @param filePath the file path from which the shapes will be loaded
+     * @return a stack of shapes loaded from the file
+     * @throws IOException if an I/O error occurs while reading the file
+     * @throws ClassNotFoundException if the class of the serialized object cannot be found
+     */
+    public static Stack<Shape> loadObjects(String filePath) throws IOException, ClassNotFoundException {
+        try (FileInputStream fis = new FileInputStream(filePath);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            return (Stack<Shape>) ois.readObject();
+        }
+    }
+}
