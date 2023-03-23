e4839bde5543aa23e16d0b5b47141f10417d83a7
rezmous <113928873+rezmous@users.noreply.github.com>
2023-03-22 19:36:26 -0700

    Delete ShapeDecorator.java

--- src/ShapeDecorator.java
+++
@@ -1,8 +0,0 @@
-public abstract class ShapeDecorator implements Decorated {
-    protected Decorated shapeDecorator;
-
-    @Override
-    public String decorate() {
-        return shapeDecorator.decorate();
-    }
-}
