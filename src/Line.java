5c82fffee95c4ccfe83535dec3ad7d7bca0998fb
Luke Fanguna <47843203+Luke-Fanguna@users.noreply.github.com>
2023-03-22 19:39:08 -0700

    Add files via upload

--- src/Line.java
+++ src/Line.java
@@ -1,18 +1,46 @@
 import java.awt.*;
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.*;
 
-public class Line implements LineInterface
-{
-    Stack<String> message;
-    Line()
-    {
-        message.push(" ");
+/**
+ * This is the line class that helps create lines between two shapes
+ *
+ * @author Anthony Colin, Shiv Panchal, Luke Fanguna, Nathan Choi, Reza Mousakhani, Luke Franks
+ */
+public class Line implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    boolean diamond = false;
+    boolean flag;
+    Stack<String> message = new Stack<>();
+    Stack<Shape> connect = new Stack<Shape>();
+
+    /**
+     * This is the constructor that helps initialize variables for the Line class
+     *
+     * @param first  shape to create the connection
+     * @param second shape to create the connection
+     */
+    Line(Shape first, Shape second) {
+        message.push("");
         message.push("true");
         message.push("false");
+        connect.push(first);
+        connect.push(second);
+
     }
-    public void draw(Graphics g)
-    {
-        Stack<Shape> connect = Repository.getR().getC();
+
+    /**
+     * This is the draw class that uses Graphics g to draw a line with an arrow head in the middle to show direction.
+     * Also, it checks if the connection starts with a diamond and if it does, we add text to it
+     *
+     * @param g the Graphics object to draw on
+     */
+    public void draw(Graphics g) {
+        String str = "";
+
         Point start = center(connect.get(0));
         Point end = center(connect.get(1));
         g.drawLine(start.x, start.y, end.x, end.y);
@@ -26,28 +54,59 @@ public class Line implements LineInterface
         int arrowSize = 10;
         int arrowAngle = 30; // in degrees
         double arrowLength = arrowSize / Math.sin(Math.toRadians(arrowAngle));
-        int arrowX1 = (int)(midX - arrowLength * Math.cos(angle - Math.toRadians(arrowAngle)));
-        int arrowY1 = (int)(midY - arrowLength * Math.sin(angle - Math.toRadians(arrowAngle)));
-        int arrowX2 = (int)(midX - arrowLength * Math.cos(angle + Math.toRadians(arrowAngle)));
-        int arrowY2 = (int)(midY - arrowLength * Math.sin(angle + Math.toRadians(arrowAngle)));
+        int arrowX1 = (int) (midX - arrowLength * Math.cos(angle - Math.toRadians(arrowAngle)));
+        int arrowY1 = (int) (midY - arrowLength * Math.sin(angle - Math.toRadians(arrowAngle)));
+        int arrowX2 = (int) (midX - arrowLength * Math.cos(angle + Math.toRadians(arrowAngle)));
+        int arrowY2 = (int) (midY - arrowLength * Math.sin(angle + Math.toRadians(arrowAngle)));
 
         // Draw the arrowhead polygon
         int[] arrowX = {midX, arrowX1, arrowX2};
         int[] arrowY = {midY, arrowY1, arrowY2};
         g.fillPolygon(arrowX, arrowY, 3);
-    }
-    private Point center(Shape x) {
-        return new Point(x.x1 + (x.x1 + x.x2) / 2, x.y1 + (x.x2 + x.y2) / 2);
-    }
-    public void lineMessage(Graphics g)
-    {
-        Stack<Shape> connect = Repository.getR().getC();
-        Point start = center(connect.get(0));
-        Point end = center(connect.get(1));
-        if (connect.get(0).equals("Diamond")) {
-            String text = message.pop();
-            g.drawString(text, (start.x-end.x)/2,(start.y-end.y)/2);
-            message.push(text);
+
+
+        if (diamond) {
+            if (connect.get(0).toString().contains("Diamond")) {
+                int[] textArrow = {midY, arrowY1 + 25, arrowY2};
+                if (flag) {
+                    Repository.getR().setTextbox("false");
+
+                } else {
+                    Repository.getR().setTextbox("true");
+                }
+            }
+        } else {
+            Repository.getR().setTextbox("");
         }
+        g.drawString(Repository.getR().getTextbox(), arrowX1, arrowY1 + 25);
+    }
+
+    /**
+     * This helps the line to point to the center of the shape
+     *
+     * @param x this is the shape object which we are making the center of
+     * @return returns a point object that is the center of the shape
+     */
+    private Point center(Shape x) {
+        return new Point(x.x1+50, x.y1+50);
+    }
+
+    /**
+     * This creates a message that changes the text on the line if the connection starts
+     * with a diamond
+     *
+     * @param cond boolean that sets the diamond
+     */
+    public void lineMessage(boolean cond) {
+        diamond = cond;
+    }
+
+    /**
+     * This sets the flag we use to alternate the text of the line
+     *
+     * @param flag boolean variable to set the flag
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
     }
 }
--- src/Repository.java
+++ src/Repository.java
@@ -1,44 +1,185 @@
 import java.util.*;
 import java.util.Observable;
+
+/**
+ * This is the repository. It stores common variables held by other classes and uses a Singleton and an Observer Pattern
+ *
+ * @author Anthony Colin, Shiv Panchal, Luke Fanguna, Nathan Choi, Reza Mousakhani, Luke Franks
+ */
 public class Repository extends Observable {
     private String shape;
+    private boolean lineFlag = false;
     private Stack<Shape> shapes = new Stack<Shape>();
     private Line line;
     private Stack<Line> lines = new Stack<Line>();
     private Stack<Shape> connection = new Stack<Shape>();
+    private Stack<Object> all = new Stack<>();
     private static Repository repo;
     private String textbox;
 
     /**
-     * allows Repository to not be changed out of scope
+     * Allows Repository to not be changed out of scope
      */
-    private Repository(){}
+    private Repository() {
+    }
+
     /**
-     * allows us to get the instance of the Respository
+     * Allows us to get the instance of the Respository
      *
      * @return instance of Repository
      */
-    public static Repository getR()
-    {
-        if (repo == null)
-        {
+    public static Repository getR() {
+        if (repo == null) {
             repo = new Repository();
         }
         return repo;
     }
-    public void setShape(String shape) {this.shape = shape;}
-    public void addShape(Shape shape) {shapes.push(shape);}
-    public void setLine(Line line) {this.line = line;}
-    public void addLine(Line line) {lines.push(line);}
-    public void setTextbox(String textbox) {this.textbox = textbox;}
-    public void addC(Shape shape){connection.push(shape);}
-    public Stack<Shape> getShapes() {return shapes;}
-    public Stack<Line> getLines() {return lines;}
-    public Stack<Shape> getC() {return connection;}
-    public void clearC() {connection.clear();}
-    public String getTextbox(){return textbox;}
-    public int getShapeSize(){return shapes.size();}
-    public Shape getElement(int i){return shapes.get(i);}
-    public String getShape(){return shape;}
 
+    /**
+     * SetLoadShapes takes the shapes from the loaded file
+     * and sets the shapes stack above with those shapes
+     *
+     * @param: stack of objects to be loaded
+     */
+    public void setLoadShapes(Stack<Shape> newShapes) {
+        shapes = newShapes;
+        setChanged();
+        notifyObservers("Load");
+    }
+
+    /**
+     * Sets the shape field to the value passed in
+     *
+     * @param shape to be used
+     */
+    public void setShape(String shape) {
+        this.shape = shape;
+    }
+
+    /**
+     * Adds a shape to the stack
+     *
+     * @param shape
+     */
+    public void addShape(Shape shape) {
+        shapes.push(shape);
+        setChanged();
+        notifyObservers("Shape");
+    }
+
+    /**
+     * Sets the line field to the value passed in
+     *
+     * @param line
+     */
+    public void setLine(Line line) {
+        this.line = line;
+    }
+
+    /**
+     * Adds the line value to the stack
+     *
+     * @param line
+     */
+    public void addLine(Line line) {
+        lines.push(line);
+        setChanged();
+        notifyObservers("Line");
+
+    }
+
+    /**
+     * Sets the textbox of the shape to the value passed in
+     *
+     * @param textbox
+     */
+    public void setTextbox(String textbox) {
+        this.textbox = textbox;
+    }
+
+    /**
+     * Adds a shape to the connection stack that helps make lines
+     *
+     * @param shape
+     */
+    public void addC(Shape shape) {
+        connection.push(shape);
+    }
+
+    /**
+     * Gets the shape stack
+     *
+     * @return stack of shapes
+     */
+    public Stack<Shape> getShapes() {
+        return shapes;
+    }
+
+    /**
+     * Gets the stack of lines
+     *
+     * @return stack of lines
+     */
+    public Stack<Line> getLines() {
+        return lines;
+    }
+
+    /**
+     * Gets the connection stack that helps build lines
+     *
+     * @return stack of shapes
+     */
+    public Stack<Shape> getC() {
+        return connection;
+    }
+
+    /**
+     * Clears the connection stack
+     */
+    public void clearC() {
+        connection.clear();
+    }
+
+    /**
+     * Gets the Textbox within a shape
+     *
+     * @return textbox of a shape
+     */
+    public String getTextbox() {
+        return textbox;
+    }
+
+
+    /**
+     * Gets the shape stored
+     *
+     * @return shape object stored
+     */
+    public String getShape() {
+        return shape;
+    }
+
+    /**
+     * Puts all the objects into one stack
+     */
+    public void setAll() {
+        for (Object object : shapes) {
+            all.push(object);
+        }
+        for (Object object : connection) {
+            all.push(object);
+        }
+        for (Object object : lines) {
+            all.push(object);
+        }
+    }
+
+    /**
+     * Puts all objects into all stack
+     *
+     * @return stack of objects
+     */
+    public Stack<Object> getAll() {
+        return all;
+    }
 }
