99e6471b8b95d4079987801aa4c48b8e5b070a6b
rezmous <113928873+rezmous@users.noreply.github.com>
2023-04-28 13:27:04 -0700

    Add files via upload

--- src/VariableDecorate.java
+++ src/VariableDecorate.java
@@ -5,18 +5,19 @@ import java.awt.*;
  *
  * @author Reza Mousakhani, Anthony Colin, Luke Fanguna, Luke Franks, Nathan Choi, & Shiv Panchal
  */
-public class VariableDecorate extends Rectangle {
-    public VariableDecorate(int x1, int y1, int WIDTH, int HEIGHT, Color c) {
-        super(x1, y1, WIDTH, HEIGHT, c);
+public class VariableDecorate extends ShapeDecorator {
+
+    public VariableDecorate(Shape decoratedShape, int x1, int y1, int WIDTH, int HEIGHT, Color c) {
+        super(decoratedShape, x1, y1, WIDTH, HEIGHT, c);
     }
 
-    /**
-     * Draws the lines for the variable object.
-     * @param g the Graphics object to draw on
-     */
     @Override
     public void draw(Graphics g) {
-        super.draw(g);
+        decoratedShape.draw(g);
+        drawVariableLines(g);
+    }
+
+    private void drawVariableLines(Graphics g) {
         int x1 = getX();
         int y1 = getY();
         int width = 100;
@@ -27,6 +28,7 @@ public class VariableDecorate extends Rectangle {
         g2d.setStroke(new BasicStroke(2));
         g.drawLine(x1 + 10, y1 + 10, x1 + width - 10, y1 + 10);
         g.drawLine(x1 + 10, y1 + 10, x1 + 10, y1 + (height + 5) - 10);
+        g.drawString(text,super.x1+35,super.y1+45);
         g2d.setStroke(oldStroke);
     }
 }
