92e4ea67a1a413431bc449c0888c0fa17fe08e75
acolinhe <acolinhe@gmail.com>
2023-04-28 19:17:47 -0700

    Added comments

--- src/DBConn.java
+++ src/DBConn.java
@@ -1,3 +1,12 @@
+
+/**
+ * The Rectangle class represents a rectangle shape that extends from the Shape class. It inherits the properties
+ * and methods of the Shape class and overrides the draw method to draw a rectangle using the given coordinates and
+ * color.
+
+ * @author Reza Mousakhani, Anthony Colin, Luke Fanguna, Luke Franks, Nathan Choi, & Shiv Panchal
+ */
+
 public class DBConn {
     String DB = System.getenv("DB");
     String USER = System.getenv("USER");
