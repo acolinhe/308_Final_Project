681184a39c950da4dea1b1b3d3f2c471aa803afb
acolinhe <acolinhe@gmail.com>
2023-04-28 19:11:35 -0700

    Updated UML... again

---
+++ src/DBConn.java
@@ -0,0 +1,6 @@
+public class DBConn {
+    String DB = System.getenv("DB");
+    String USER = System.getenv("USER");
+    String HOST = System.getenv("HOST");
+    String PASS = System.getenv("PASSWORD");
+}
